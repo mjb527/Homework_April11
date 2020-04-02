7f2a7a23e0dc5e96497cfdface4fe4062739fbc4
Matt <mbatko0914@gmail.com>
2020-04-02 17:08:49 -0400

    fixed up some js

--- static/script.js
+++ static/script.js
@@ -2,17 +2,20 @@
 
 $(document).ready(function(event) {
 	const payload = {
-		'url' : 'https://national-weather-service.p.rapidapi.com/products/types/%7BtypeId%7D',
-		'method' : 'GET',
-		'x-rapidapi-host' : "national-weather-service.p.rapidapi.com",
-		'x-rapidapi-key' : "6483eaca41mshb54c1d2e3ae9a30p1848d0jsnc2a733ce6052"
-	};
+	"async": true,
+	"crossDomain": true,
+	"url": "https://national-weather-service.p.rapidapi.com/products/types/%7BtypeId%7D",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "national-weather-service.p.rapidapi.com",
+		"x-rapidapi-key": "483eaca41mshb54c1d2e3ae9a30p1848d0jsnc2a733ce6052"
+		}
+	}
 
 	$.ajax(payload).then(function(response) {
 		console.log(response);
-		}
+		
 	});
-
 
 
 });
