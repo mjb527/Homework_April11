8cdf0fd8f9850e306cbb269796436091939c4d40
Matt <mbatko0914@gmail.com>
2020-04-02 17:05:09 -0400

    touched some stuff up, broke javascript

--- static/script.js
+++ static/script.js
@@ -1,18 +1,18 @@
-var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://community-open-weather-map.p.rapidapi.com/forecast?q=san%20francisco%252Cus&units=standard&lang=en",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-		"x-rapidapi-key": "6483eaca41mshb54c1d2e3ae9a30p1848d0jsnc2a733ce6052"
-	}
-}
 
-$(document).ready(function() {
-  $.ajax(settings).done(function (response) {
-  	console.log(response);
-  });
+
+$(document).ready(function(event) {
+	const payload = {
+		'url' : 'https://national-weather-service.p.rapidapi.com/products/types/%7BtypeId%7D',
+		'method' : 'GET',
+		'x-rapidapi-host' : "national-weather-service.p.rapidapi.com",
+		'x-rapidapi-key' : "6483eaca41mshb54c1d2e3ae9a30p1848d0jsnc2a733ce6052"
+	};
+
+	$.ajax(payload).then(function(response) {
+		console.log(response);
+		}
+	});
+
 
 
 });
