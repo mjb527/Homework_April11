714e5f41041077d0b464e8ea4bc8acc1f97e92b8
Matt <mbatko0914@gmail.com>
2020-04-02 18:15:55 -0400

    adjust media query

--- static/script.js
+++ static/script.js
@@ -1,25 +1,12 @@
 
 
 $(document).ready(function(event) {
-	// const payload = {
-	// "async": true,
-	// "crossDomain": true,
-	// "url": "https://national-weather-service.p.rapidapi.com/products/locations",
-	// "method": "GET",
-	// "headers": {
-	// 	"x-rapidapi-host": "national-weather-service.p.rapidapi.com",
-	// 	"x-rapidapi-key": "6483eaca41mshb54c1d2e3ae9a30p1848d0jsnc2a733ce6052"
-	// 	}
-	// }
-	//
-	// $.ajax(payload).then(function(response) {
-	// 	console.log(response);
-	//
-	// });
+
+	console.log('got here');
 
 	let cityName = 'Berlin';
 
-	const url = 'api.openweathermap.org/data/2.5/forecast/daily?q=' + cityName + '&cnt=6&appid=0c975a7e003d3a7d785c46c579b0d823';
+	const url = 'https://api.openweathermap.org/data/2.5/forecast/daily?q=' + cityName + '&cnt=6&appid=0c975a7e003d3a7d785c46c579b0d823';
 
 	$.ajax(url).then(function (response) {
 		console.log(response);
