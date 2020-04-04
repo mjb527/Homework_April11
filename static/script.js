13bb50ed31ec80608078f20f658d178575ebd7b2
Matt <mbatko0914@gmail.com>
2020-04-04 12:24:38 -0400

    changed jQuery on badges to be more dynamic

--- static/script.js
+++ static/script.js
@@ -15,9 +15,19 @@ $(document).ready(function(event) {
 		let cityName = formatCityName($('#city-input').val());
 		if(cityName === null || cityName === undefined) return;
 
-		prependList(cityName);
 		request(cityName);
 
+	});
+
+	// when typing in the search box, search on enter click
+	$('#city-input').focus(function() {
+		$('#city-input').keypress(function(event) {
+			if(event.charCode === 13) {
+				let cityName = formatCityName($('#city-input').val());
+				if(cityName === null || cityName === undefined) return;
+				request(cityName);
+			}
+		});
 	});
 
 	$('.list-group-item').click(function() {
@@ -48,16 +58,15 @@ $(document).ready(function(event) {
 
 	}
 	// delete button behavior
-	$('.delete').hover(function() {
+	$(document).on('mouseover', '.delete', function() {
 		$(this).addClass('badge-danger');
 		$(this).removeClass('badge-secondary');
-	},
-	function() {
+	}).on('mouseout', '.delete',	function() {
 		$(this).addClass('badge-secondary');
 		$(this).removeClass('badge-danger');
 	});
 
-	$('.delete').click(function() {
+	$(document).on('click', '.delete', function() {
 		const parent = $(this).closest('.list-group-item');
 		if(confirm(`Delete ${parent.attr('data-val')} from the list of cities?`)) {
 			// remove from cities array
@@ -69,19 +78,42 @@ $(document).ready(function(event) {
 
 		}
 
-	})
+	});
 
 	function prependList(city) {
-		$('#cities').prepend('<a href="#" class="list-group-item list-group-item-action" data-city="' + city + '">' + city + '</a>');
+		$('.selected').removeClass('selected');
+		$('#cities').prepend(`<a href="#" class="list-group-item list-group-item-action selected" data-val="${city}"><div class="row">
+						<div class="col-10">${city}</div>
+						<div class="col-1"><span class="badge badge-secondary badge-pill delete"><i class="far fa-trash-alt"></i></span></div>
+						</div>
+					</a>
+				`);
 		citiesArr.push(city);
 		localStorage.setItem('cities', citiesArr.toString());
 	}
 
 	function request(city) {
+		// clear the input field
+		$('#city-input').val('');
 		const apiKey = '0c975a7e003d3a7d785c46c579b0d823';
 		const url = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + apiKey;
 
-		$.ajax(url).then(function (response) {
+		$.ajax(url).then(function(response) {
+			// if the city isn't already on the list, add it
+			if(citiesArr.indexOf(city) === -1) {
+				prependList(city);
+			}
+			// otherwise, find the list item and add class to it
+			else {
+				// const cityList = ;
+				$.each($('.list-group-item'), function(i, val) {
+					if($(val).attr('data-val') === city) {
+						$('.selected').removeClass('selected');
+						$(val).addClass('selected');
+					}
+				});
+			}
+
 			// set the parameters based on the first object
 			const obj = response.list['0'];
 			const weather = obj.weather['0'].main;
@@ -132,13 +164,13 @@ $(document).ready(function(event) {
 			const box = $('<div>');
 			box.addClass("bg-primary text-white forecast-box px-2 col-md-2 col-sm-12 mb-2 mt-1 mx-auto");
 			box.html(`
-				<div class="date pt-1 h5 row">
+				<div class="date pt-1 pl-2 h5 row">
 							<div class="col-8"><strong>${date}</strong</div>
 							<div class="col-4">${img}</div>
 				</div>
 				<div class="smaller weatherDiv pb-1 px-2">Weather: <span class="weatherSpan">${weather}</span></div>
 				<div class="smaller tempDiv pb-1 px-2">Temperature: <span class="tempSpan">${temperature + String.fromCharCode(176)}F</span></div>
-				<div class="smaller humidityDiv pb-1">Humidity: <span class="humiditySpan">${humidity}%</span></div>
+				<div class="smaller humidityDiv pb-1 px-2">Humidity: <span class="humiditySpan">${humidity}%</span></div>
 				<div class="smaller windDiv pb-1 px-2">Wind: <span class="windSpan">${wind} mph</span></div>
 			`);
 
@@ -158,16 +190,13 @@ $(document).ready(function(event) {
 
 	// set the name of the city properly and aVOid tHiS tyPe of TYPinG
 	function formatCityName(name) {
-		console.log(name);
 		// set all to be lowercase
 		name = name.toLowerCase();
 		// set first letter capital
 		name = name.charAt(0).toUpperCase() + name.slice(1);
-		console.log(name);
 		// find each new letter next to a space, hyphen, or period and make capital
 		const names = name.split('');
 		for(let i = 0; i < name.length; i++) {
-			console.log(names[i]);
 			if(names[i] === ' ' || names[i] === '-')
 				names[i+1] = names[i+1].toUpperCase();
 			if(names[i] === '.')
